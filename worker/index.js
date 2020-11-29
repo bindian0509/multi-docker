3a77cafdcecb48f68698f3cbbfd953bc98d293b5
Bharat Verma <bindian0509@gmail.com>
2020-11-30 00:35:58 +0530

    fib function change

--- worker/index.js
+++ worker/index.js
@@ -8,9 +8,18 @@ const redisClient = redis.createClient({
 });
 const sub = redisClient.duplicate();
 
-function fib(index) {
-  if (index < 2) return 1;
-  return fib(index - 1) + fib(index - 2);
+function fib(num) {
+    var num1=0; 
+    var num2=1; 
+    var sum; 
+    var i=0; 
+    for (i = 0; i < num; i++)  
+    { 
+        sum=num1+num2; 
+        num1=num2; 
+        num2=sum; 
+    } 
+    return num2; 
 }
 
 sub.on('message', (channel, message) => {
